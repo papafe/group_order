eb523c489ce9971ecadf05e8f3f0be9a6d06733a
Ferdinando Papale <>
2022-07-23 10:58:38 +0200

    Added a consideration

--- lib/pages/order_page.dart
+++ lib/pages/order_page.dart
@@ -8,6 +8,35 @@ import 'package:group_order/viewmodel/immutable_v2.dart';
 import 'package:group_order/viewmodel/order_list_view_model.dart';
 import 'package:realm/src/results.dart';
 
+/* Summing up:
+
+- The viewModel that uses ChangeProvider is probably the one that feels the most
+natural to me. We just force an update when we need
+- The first immutable version definitely works, and it's a 1-to-1 substitution in 
+the ui (this page). The issue is that the view model depends on the changes stream,
+so it gets rebuilt every time there is a change in the realm collection. This means
+that we can't keep state in the view model, otherwise it gets overwritten all the time
+- The second immutable version makes more sense, because the view model is independent from
+the changes in the collection. But in order to make this work we need to add
+a watch to the menuItemsStreamProvider in the view, so that it gets redrawn on change
+of the collection, while the view model is the same. This can be verified by 
+modifying the value of "fakeState" and printing it.
+
+Considerations
+- This still feels kinda awkward, but it seems a common issue with state management
+in flutter anyways
+- If realm provided a stream that would contain not only the changes, but the new 
+collection too, then for simple cases we could just make a stream provider and use that
+in the view, without the need to do like in v2 (where we need to have 2 providers necessarily,
+one for the update and one for the current state). Maybe if it provided the realm too, then we'd
+be golden, because almost everything realm related can be done with that.
+- Probably the realm provider should have the .autoDispose modifier
+- Using the .family modifier probably we can make the stream provider for collection
+more generic (so we avoid creating n+1 providers for different collections and 
+filters).
+
+*/
+
 class OrderPage extends ConsumerWidget {
   const OrderPage({Key? key}) : super(key: key);
 
